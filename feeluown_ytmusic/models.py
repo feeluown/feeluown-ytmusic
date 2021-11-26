392219e01db21359b46a8a9cc375c5b071241940
Bruce Zhang <zttt183525594@gmail.com>
2021-11-26 09:47:41 +0800

    service: implements artist & user info

--- feeluown_ytmusic/models.py
+++ feeluown_ytmusic/models.py
@@ -34,26 +34,28 @@ class SearchNestedThumbnail(BaseModel):
     height: int
 
 
-class YtmusicSearchSong(BaseModel):
+class YtmusicSearchBase(BaseModel):
+    category: str
+    resultType: str
+
+
+class YtmusicSearchSong(YtmusicSearchBase):
     class Album(BaseModel):
         id: str
         name: str
 
-    category: str
-    resultType: str
     title: str  # 歌名
     album: Album  # 专辑信息
     feedbackTokens: dict
     videoId: str  # 歌曲ID
     duration: str  # 歌曲时长 eg.3:50
     artists: List[SearchNestedArtist]  # 歌手信息
+    isAvailable: bool
     isExplicit: bool
     thumbnails: List[SearchNestedThumbnail]  # 封面信息
 
 
-class YtmusicSearchAlbum(BaseModel):
-    category: str
-    resultType: str
+class YtmusicSearchAlbum(YtmusicSearchBase):
     title: str  # 专辑名
     type: str  # 专辑类型
     year: int  # 年
@@ -63,9 +65,7 @@ class YtmusicSearchAlbum(BaseModel):
     thumbnails: List[SearchNestedThumbnail]  # 封面信息
 
 
-class YtmusicSearchArtist(BaseModel):
-    category: str
-    resultType: str
+class YtmusicSearchArtist(YtmusicSearchBase):
     artist: str  # 歌手名
     shuffleId: str
     radioId: str
@@ -73,9 +73,7 @@ class YtmusicSearchArtist(BaseModel):
     thumbnails: List[SearchNestedThumbnail]  # 封面信息
 
 
-class YtmusicSearchPlaylist(BaseModel):
-    category: str
-    resultType: str
+class YtmusicSearchPlaylist(YtmusicSearchBase):
     title: str  # 歌单名
     itemCount: int  # 歌曲数量
     author: str  # 歌单作者
@@ -83,12 +81,81 @@ class YtmusicSearchPlaylist(BaseModel):
     thumbnails: List[SearchNestedThumbnail]  # 封面信息
 
 
-class YtmusicSearchVideo(BaseModel):
-    category: str
-    resultType: str
+class YtmusicSearchVideo(YtmusicSearchBase):
     title: str  # 视频标题
     views: str  # 播放量 eg:13K
     videoId: str  # 视频ID
     duration: str  # 视频时长 eg.3:50
     artists: List[SearchNestedArtist]  # 歌手信息
     thumbnails: List[SearchNestedThumbnail]  # 封面信息
+    playlistId: str
+
+
+class YtmusicDispatcher:
+    RESULT_TYPE_MAP = {
+        'video': YtmusicSearchVideo,
+        'song': YtmusicSearchSong,
+        'artist': YtmusicSearchArtist,
+        'album': YtmusicSearchAlbum,
+        'playlist': YtmusicSearchPlaylist,
+    }
+
+    @classmethod
+    def search_result_dispatcher(cls, **data) \
+            -> Union[YtmusicSearchBase, YtmusicSearchVideo, YtmusicSearchSong, YtmusicSearchArtist, YtmusicSearchAlbum,
+                     YtmusicSearchPlaylist]:
+        clazz = cls.RESULT_TYPE_MAP.get(data.get('resultType'))
+        return clazz(**data) if clazz is not None else YtmusicSearchBase(**data)
+
+
+class ArtistInfo(BaseModel):
+    class Songs(BaseModel):
+        browseId: str  # 查询ID
+        results: List[YtmusicSearchSong]  # 歌曲列表（部分）
+
+    class Albums(BaseModel):
+        browseId: str  # 查询ID
+        results: List[YtmusicSearchAlbum]  # 专辑列表（部分）
+        params: str  # 不知道是啥
+
+    class Videos(BaseModel):
+        browseId: str  # 查询ID
+        results: List[YtmusicSearchVideo]  # 视频列表（部分）
+
+    class RelatedArtists(BaseModel):
+        results: List[YtmusicSearchArtist]  # 相关歌手
+
+    name: str  # 歌手名
+    description: str  # 描述
+    views: str  # 播放量 eg.177,865,792 views
+    channelId: str
+    shuffleId: str
+    radioId: str
+    subscribers: str  # 订阅量 eg.230K
+    subscribed: bool  # 是否已订阅
+    thumbnails: List[SearchNestedThumbnail]  # 封面信息
+    songs: Songs
+    albums: Albums  # 专辑
+    singles: Albums  # 单曲专辑
+    videos: Videos
+    related: RelatedArtists
+
+
+class UserInfo(BaseModel):
+    class Playlists(BaseModel):
+        class PlaylistResult(BaseModel):
+            title: str
+            playlistId: str
+            thumbnails: List[SearchNestedThumbnail]
+
+        browseId: str
+        results: List[PlaylistResult]
+        params: str
+
+    class Videos(BaseModel):
+        browseId: str  # 查询ID
+        results: List[YtmusicSearchVideo]  # 视频列表（部分）
+
+    name: str
+    playlists: Playlists
+    videos: Videos
--- feeluown_ytmusic/service.py
+++ feeluown_ytmusic/service.py
@@ -3,7 +3,7 @@ from typing import Optional, Union, List
 
 from feeluown_ytmusic.consts import HEADER_FILE
 from feeluown_ytmusic.models import YtmusicSearchSong, YtmusicSearchAlbum, YtmusicSearchArtist, YtmusicSearchVideo, \
-    YtmusicSearchPlaylist
+    YtmusicSearchPlaylist, YtmusicSearchBase, YtmusicDispatcher, ArtistInfo, UserInfo
 from ytmusicapi import YTMusic
 
 
@@ -20,15 +20,6 @@ class YtmusicScope(Enum):
     up = 'uploads'
 
 
-YTMUSIC_SEARCH_MAP = {
-    YtmusicType.so: YtmusicSearchSong,
-    YtmusicType.vi: YtmusicSearchVideo,
-    YtmusicType.ar: YtmusicSearchArtist,
-    YtmusicType.al: YtmusicSearchAlbum,
-    YtmusicType.pl: YtmusicSearchPlaylist,
-}
-
-
 class YtmusicService:
     def __init__(self):
         if HEADER_FILE.exists():
@@ -38,19 +29,31 @@ class YtmusicService:
 
     def search(self, keywords: str, t: Optional[YtmusicType], scope: Optional[YtmusicScope], page_size: int = 20) \
             -> List[Union[YtmusicSearchSong, YtmusicSearchAlbum, YtmusicSearchArtist, YtmusicSearchVideo,
-                          YtmusicSearchPlaylist, None]]:
-        clazz = YTMUSIC_SEARCH_MAP.get(t)
-        if clazz is None:
-            return []
+                          YtmusicSearchPlaylist, YtmusicSearchBase]]:
         response = self.api.search(keywords, None if t is None else t.value, None if scope is None else scope.value,
                                    page_size)
         if response is None:
             return []
-        return [clazz(**data) for data in response]
+        return [YtmusicDispatcher.search_result_dispatcher(**data) for data in response]
+
+    def artist_info(self, channel_id: str) -> ArtistInfo:
+        return ArtistInfo(**self.api.get_artist(channel_id))
+
+    def artist_albums(self, channel_id: str, params: str) -> List[YtmusicSearchAlbum]:
+        response = self.api.get_artist_albums(channel_id, params)
+        if response is None:
+            return []
+        return [YtmusicSearchAlbum(**data) for data in response]
+
+    def user_info(self, channel_id: str) -> UserInfo:
+        return UserInfo(**self.api.get_user(channel_id))
+
+    def user_playlists(self, channel_id: str, params: str):
+        return self.api.get_user_playlists(channel_id, params)
 
 
 if __name__ == '__main__':
     import json
 
     service = YtmusicService()
-    print(json.dumps(service.search('阿梓', YtmusicType.vi, None, 10)))
+    print(service.user_info('UCpbIcwkTYzRDIfm1l4E5YMg'))
