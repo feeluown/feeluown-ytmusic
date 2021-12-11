6af245e2997f8265c3d6c1cac0ca327dc7606d4b
Bruce Zhang <zttt183525594@gmail.com>
2021-12-11 23:36:37 +0800

    ui: fix qml backend

--- fuo_ytmusic/page_explore_qml.py
+++ fuo_ytmusic/page_explore_qml.py
@@ -1,17 +1,10 @@
-import asyncio
 from pathlib import Path
 
-from PyQt5.QtCore import Qt, QRect, QPoint, QSize, QMargins, QUrl, QObject, pyqtProperty
+from PyQt5.QtCore import Qt, QUrl, QObject, pyqtProperty
 from PyQt5.QtQuick import QQuickView
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QButtonGroup, QPushButton, QSizePolicy, QLayout, \
-    QTabWidget, QAbstractButton
-from feeluown.gui.helpers import BgTransparentMixin, fetch_cover_wrapper
-from feeluown.gui.widgets.playlist import PlaylistListView, PlaylistListModel, PlaylistFilterProxyModel
-from feeluown.utils.reader import wrap
+from PyQt5.QtWidgets import QWidget
 
 from fuo_ytmusic import YtmusicProvider
-from fuo_ytmusic.helpers import Singleton
-from fuo_ytmusic.models import Categories
 
 
 class ExploreBackend(QObject):
@@ -19,34 +12,25 @@ class ExploreBackend(QObject):
         super().__init__()
         self._provider = provider
 
-    @pyqtProperty(bool, constant=True)
-    def categories(self) -> Categories:
-        return self._provider.categories()
+    @pyqtProperty('QVariantMap', constant=True)
+    def categories(self) -> dict:
+        result = dict()
+        categories = self._provider.categories()
+        result['forYou'] = [{'title': c.title, 'params': c.params} for c in categories.forYou]
+        result['moods'] = [{'title': c.title, 'params': c.params} for c in categories.moods]
+        result['genres'] = [{'title': c.title, 'params': c.params} for c in categories.genres]
+        return result
 
 
 async def render(req, **_):
     app = req.ctx['app']
     provider = app.library.get('ytmusic')
     view = QQuickView()
-    view.rootContext().setContextProperty('explore_backend', ExploreBackend(provider))
+    app._ytmusic_explore_backend = ExploreBackend(provider)
+    # noinspection PyProtectedMember
+    view.rootContext().setContextProperty('explore_backend', app._ytmusic_explore_backend)
     container = QWidget.createWindowContainer(view, app.ui.right_panel)
     container.setFocusPolicy(Qt.TabFocus)
     view.setResizeMode(QQuickView.SizeRootObjectToView)
     view.setSource(QUrl.fromLocalFile((Path(__file__).parent / 'qml' / 'page_explore.qml').as_posix()))
-
-    # async def select_playlist(btn: QAbstractButton):
-    #     params: str = btn.property('params')
-    #     loop = asyncio.get_event_loop()
-    #     playlists = await loop.run_in_executor(None, provider.category_playlists, params)
-    #     model = PlaylistListModel(wrap(playlists),
-    #                               fetch_cover_wrapper(app.img_mgr),
-    #                               {p.identifier: p.name for p in app.library.list()})
-    #     filter_model = PlaylistFilterProxyModel()
-    #     filter_model.setSourceModel(model)
-    #     view.playlist_list_view.setModel(filter_model)
-
-    # view.recommand_view.button_group.buttonPressed.connect(lambda _: asyncio.ensure_future(select_playlist(_)))
-
-    # view.playlist_list_view.show_playlist_needed.connect(
-    #     lambda model: app.browser.goto(model=model))
     app.ui.right_panel.set_body(container)
--- fuo_ytmusic/qml/dummydata/explore_backend.qml
+++ fuo_ytmusic/qml/dummydata/explore_backend.qml
@@ -2,5 +2,28 @@ import QtQuick 2.3
 
 // 以下是模拟数据用于 qmlscene 加载
 QtObject {
-    property string test: "Hello World"
+    QtObject {
+        id: categories
+        ListModel {
+            id: forYou
+            ListElement {
+                title: "For You Playlist"
+                params: "test for you params"
+            }
+        }
+        ListModel {
+            id: moods
+            ListElement {
+                title: "Moods Playlist"
+                params: "Moods params"
+            }
+        }
+        ListModel {
+            id: genres
+            ListElement {
+                title: "Genres Playlist"
+                params: "Genres params"
+            }
+        }
+    }
 }
